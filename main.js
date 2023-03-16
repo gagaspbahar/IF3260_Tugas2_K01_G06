92494535db0705e055f174533c1eeca3e3683338
Gagas Praharsa Bahar <gagaspbahar@gmail.com>
2023-03-17 00:32:51 +0700

    init F shape

---
+++ main.js
@@ -0,0 +1,199 @@
+var translation = [45, 150, 0];
+var rotation = [degToRad(40), degToRad(25), degToRad(325)];
+var scale = [1, 1, 1];
+
+// Initialize the WebGL context
+var canvas = document.querySelector("#gl-canvas");
+var gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+if (!gl) {
+  alert("WebGL not available");
+}
+
+// Create, upload, and compile the shaders
+var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+// Link the two shaders above into a program
+var program = createProgram(gl, vertexShader, fragmentShader);
+
+gl.useProgram(program);
+
+var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+var colorUniformLocation = gl.getUniformLocation(program, "u_color");
+var colorAttributeLocation = gl.getAttribLocation(program, "a_color");
+var matrixLocation = gl.getUniformLocation(program, "u_matrix");
+
+// Set viewport
+gl.viewport(0, 0, canvas.width, canvas.height);
+
+
+
+function render() {
+  var buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  setGeometry(gl);
+  gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+
+  var colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  setColors(gl);
+  gl.vertexAttribPointer(colorAttributeLocation, 3, gl.UNSIGNED_BYTE, true, 0, 0);
+  gl.enableVertexAttribArray(colorAttributeLocation);
+}
+
+function drawScene() {
+  // Clear the canvas
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Draw a rectangle
+  var offsetNow = 0;
+  var countNow = 0;
+  gl.enable(gl.CULL_FACE);
+  gl.enable(gl.DEPTH_TEST);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  
+  render();
+
+  var matrix = m4.projection(gl.canvas.clientWidth, gl.canvas.clientHeight, 400);
+  matrix = m4.translate(matrix, translation[0], translation[1], translation[2]);
+  matrix = m4.xRotate(matrix, rotation[0]);
+  matrix = m4.yRotate(matrix, rotation[1]);
+  matrix = m4.zRotate(matrix, rotation[2]);
+  matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
+
+  gl.uniformMatrix4fv(matrixLocation, false, matrix);
+  gl.drawArrays(gl.TRIANGLES, 0, 16 * 6);
+}
+
+function setGeometry(gl) {
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      // left column front
+      0, 0, 0, 0, 150, 0, 30, 0, 0, 0, 150, 0, 30, 150, 0, 30, 0, 0,
+
+      // top rung front
+      30, 0, 0, 30, 30, 0, 100, 0, 0, 30, 30, 0, 100, 30, 0, 100, 0, 0,
+
+      // middle rung front
+      30, 60, 0, 30, 90, 0, 67, 60, 0, 30, 90, 0, 67, 90, 0, 67, 60, 0,
+
+      // left column back
+      0, 0, 30, 30, 0, 30, 0, 150, 30, 0, 150, 30, 30, 0, 30, 30, 150, 30,
+
+      // top rung back
+      30, 0, 30, 100, 0, 30, 30, 30, 30, 30, 30, 30, 100, 0, 30, 100, 30, 30,
+
+      // middle rung back
+      30, 60, 30, 67, 60, 30, 30, 90, 30, 30, 90, 30, 67, 60, 30, 67, 90, 30,
+
+      // top
+      0, 0, 0, 100, 0, 0, 100, 0, 30, 0, 0, 0, 100, 0, 30, 0, 0, 30,
+
+      // top rung right
+      100, 0, 0, 100, 30, 0, 100, 30, 30, 100, 0, 0, 100, 30, 30, 100, 0, 30,
+
+      // under top rung
+      30, 30, 0, 30, 30, 30, 100, 30, 30, 30, 30, 0, 100, 30, 30, 100, 30, 0,
+
+      // between top rung and middle
+      30, 30, 0, 30, 60, 30, 30, 30, 30, 30, 30, 0, 30, 60, 0, 30, 60, 30,
+
+      // top of middle rung
+      30, 60, 0, 67, 60, 30, 30, 60, 30, 30, 60, 0, 67, 60, 0, 67, 60, 30,
+
+      // right of middle rung
+      67, 60, 0, 67, 90, 30, 67, 60, 30, 67, 60, 0, 67, 90, 0, 67, 90, 30,
+
+      // bottom of middle rung.
+      30, 90, 0, 30, 90, 30, 67, 90, 30, 30, 90, 0, 67, 90, 30, 67, 90, 0,
+
+      // right of bottom
+      30, 90, 0, 30, 150, 30, 30, 90, 30, 30, 90, 0, 30, 150, 0, 30, 150, 30,
+
+      // bottom
+      0, 150, 0, 0, 150, 30, 30, 150, 30, 0, 150, 0, 30, 150, 30, 30, 150, 0,
+
+      // left side
+      0, 0, 0, 0, 0, 30, 0, 150, 30, 0, 0, 0, 0, 150, 30, 0, 150, 0,
+    ]),
+    gl.STATIC_DRAW
+  );
+}
+// Fill the buffer with colors for the 'F'.
+function setColors(gl) {
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Uint8Array([
+      // left column front
+      200, 70, 120, 200, 70, 120, 200, 70, 120, 200, 70, 120, 200, 70, 120, 200,
+      70, 120,
+
+      // top rung front
+      200, 70, 120, 200, 70, 120, 200, 70, 120, 200, 70, 120, 200, 70, 120, 200,
+      70, 120,
+
+      // middle rung front
+      200, 70, 120, 200, 70, 120, 200, 70, 120, 200, 70, 120, 200, 70, 120, 200,
+      70, 120,
+
+      // left column back
+      80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70,
+      200,
+
+      // top rung back
+      80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70,
+      200,
+
+      // middle rung back
+      80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70,
+      200,
+
+      // top
+      70, 200, 210, 70, 200, 210, 70, 200, 210, 70, 200, 210, 70, 200, 210, 70,
+      200, 210,
+
+      // top rung right
+      200, 200, 70, 200, 200, 70, 200, 200, 70, 200, 200, 70, 200, 200, 70, 200,
+      200, 70,
+
+      // under top rung
+      210, 100, 70, 210, 100, 70, 210, 100, 70, 210, 100, 70, 210, 100, 70, 210,
+      100, 70,
+
+      // between top rung and middle
+      210, 160, 70, 210, 160, 70, 210, 160, 70, 210, 160, 70, 210, 160, 70, 210,
+      160, 70,
+
+      // top of middle rung
+      70, 180, 210, 70, 180, 210, 70, 180, 210, 70, 180, 210, 70, 180, 210, 70,
+      180, 210,
+
+      // right of middle rung
+      100, 70, 210, 100, 70, 210, 100, 70, 210, 100, 70, 210, 100, 70, 210, 100,
+      70, 210,
+
+      // bottom of middle rung.
+      76, 210, 100, 76, 210, 100, 76, 210, 100, 76, 210, 100, 76, 210, 100, 76,
+      210, 100,
+
+      // right of bottom
+      140, 210, 80, 140, 210, 80, 140, 210, 80, 140, 210, 80, 140, 210, 80, 140,
+      210, 80,
+
+      // bottom
+      90, 130, 110, 90, 130, 110, 90, 130, 110, 90, 130, 110, 90, 130, 110, 90,
+      130, 110,
+
+      // left side
+      160, 160, 220, 160, 160, 220, 160, 160, 220, 160, 160, 220, 160, 160, 220,
+      160, 160, 220,
+    ]),
+    gl.STATIC_DRAW
+  );
+}
+
+drawScene();
---
+++ shader.js
@@ -0,0 +1,48 @@
+var vertexShaderSource = `
+attribute vec4 a_position;
+uniform mat4 u_matrix;
+uniform vec2 u_resolution;
+attribute vec4 a_color;
+varying vec4 v_color;
+
+void main() {
+  gl_Position = u_matrix * a_position;
+  v_color = a_color;
+}
+`;
+
+var fragmentShaderSource = `
+precision mediump float;
+varying vec4 v_color;
+void main() {
+  gl_FragColor = v_color;
+}`;
+
+// Create a shader object, upload the source and compile the shader.
+function createShader(gl, type, source) {
+  var shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      console.log("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
+      alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
+      gl.deleteShader(shader);
+      return null;
+  }
+  return shader;
+}
+
+// Create the shader program.
+function createProgram(gl, vertexShader, fragmentShader) {
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      console.log("Unable to initialize the shader program: " + gl.getProgramInfoLog(program));
+      alert("Unable to initialize the shader program: " + gl.getProgramInfoLog(program));
+      gl.deleteProgram(program);
+      return null;
+  }
+  return program;
+}
